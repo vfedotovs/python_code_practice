9ed65d7df337d6023745e312eb722b113e61c731
vfedotovs <39173395+vfedotovs@users.noreply.github.com>
2019-12-30 16:18:17 +0000

    class needed for payroll main.py app

---
+++ employee.py
@@ -0,0 +1,7 @@
+class Employee():
+	def __init__(self,name,title,tax_id,salary,child_count):
+		self.name = name
+		self.title = title
+		self.tax_id = tax_id
+		self.salary = salary  # weekly wage
+		self.child_count = child_count
